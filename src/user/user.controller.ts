d15eff50b58eb0b56549f497720e43a6eeb80ef3
BaeJia <qowldk1030@gmail.com>
2024-02-02 23:41:32 +0900

    feat : user 서비스

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('user') // ❶ 컨트롤러 설정 데코레이터
+export class UserController {
+  constructor(private userService: UserService) {} // ❷ 유저 서비스를주입
+
+  @Post('/create')
+  createUser(@Body() user: User) {
+    // ❸ 유저 생성
+    return this.userService.createUser(user);
+  }
+
+  @Get('/getUser/:email')
+  async getUser(@Param('email') email: string) {
+    // ❹ 한 명의 유저 찾기
+    const user = await this.userService.getUser(email);
+    console.log(user);
+    return user;
+  }
+
+  @Put('/update/:email')
+  updateUser(@Param('email') email: string, @Body() user: User) {
+    // ❺ 유저 정보 업데이트
+    console.log(user);
+    return this.userService.updateUser(email, user);
+  }
+
+  @Delete('/delete/:email')
+  deleteUser(@Param('email') email: string) {
+    // ❻ 유저 삭제
+    return this.userService.deleteUser(email);
+  }
+}
